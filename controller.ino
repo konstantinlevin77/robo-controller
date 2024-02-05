e32bbdab5223492fe8460193de815741f5ee77e4
Mehmet <tekmanmehmetyasar@protonmail.com>
2024-02-05 22:44:55 +0300

    Controller code except bluetooth is ready.

--- controller.ino
+++ controller.ino
@@ -13,9 +13,14 @@ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
+#include <SoftwareSerial.h>
+
 #define MOVEMENT_CTRL_VERT A0
 #define MOVEMENT_CTRL_HORZ A1
 
+
+// For the arms vertical values will be for elbows
+// horizontal values will be for arms and button will move the shoulder.
 #define LEFT_CTRL_VERT A2
 #define LEFT_CTRL_HORZ A3
 #define LEFT_CTRL_BUT 2
@@ -23,6 +28,7 @@ TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR TH
 #define RIGHT_CTRL_VERT A4
 #define RIGHT_CTRL_HORZ A5
 #define RIGHT_CTRL_BUT 3
+
 
 #define HUM_TO_VEHIC_BUT 4
 
@@ -32,11 +38,22 @@ or to the down, the value gets smaller. When you push it to the right or to the 
 These are the threshold values in order to determine in which directions the controller is pushed to.
 */
 #define L_MOV_THRESHOLD 400
-#define R_MOV_THRESHOLD 800
-#define F_MOV_THRESHOLD 800
+#define R_MOV_THRESHOLD 600
+#define F_MOV_THRESHOLD 600
 #define B_MOV_THRESHOLD 400
 
+/*
+The button of the joystick module I have has some sensivity problems. It clicks a few times no matter how soft I push. 
+Thus we've decided to increase the angle a little bit so that it won't make so much difference.
+*/
+#define SHOULDER_ANGLE_INCREASE 1.5
+
 bool isHumanoid = true;
+
+int leftShoulderAngle = 30;
+int rightShoulderAngle = 30;
+
+SoftwareSerial mySerial(10, 11); // RX, TX
 
 String determineMovementVert(int movY) {
 
@@ -81,33 +98,167 @@ String determineMovementHorz(int movX) {
 
 }
 
+
+String determineElbowMovement(String elbow, int ctrlY) {
+
+  String cmd = "";
+
+  // This is to make sure that elbow only moves when controller is actually used.
+  // sometimes potentiometers return values close to 512 even though they're not actually used.
+  if ((ctrlY <= 490 || ctrlY >= 530) && isHumanoid) {
+
+
+    int elbowAngle = map(ctrlY,0,1023,-118,118);
+    cmd = elbow + "-ELBOW "+ String(elbowAngle);
+  }
+
+  return cmd;
+
+}
+
+
+String determineArmMovement(String arm, int ctrlX) {
+
+  String cmd = "";
+
+  // This is to make sure that elbow only moves when controller is actually used.
+  // sometimes potentiometers return values close to 512 even though they're not actually used.
+  if ((ctrlX <= 490 || ctrlX >= 530) && isHumanoid) {
+
+    int elbowAngle = map(ctrlX,0,1023,-118,118);
+    cmd = arm + "-ARM "+ String(elbowAngle);
+  }
+
+  return cmd;
+
+}
+
+
+String determineShoulderMovement(bool isLeftShoulder, int ctrlBut) {
+
+  // Button returns 1 when not pressed, 0 when pressed.
+  if (ctrlBut == 0 && isHumanoid) {
+
+
+    if (isLeftShoulder) {
+
+      if (leftShoulderAngle >= 90) {
+        leftShoulderAngle = 30;
+      }
+      else{
+        leftShoulderAngle = leftShoulderAngle + SHOULDER_ANGLE_INCREASE;
+      }
+      return "LEFT-SHOULDER " + String(leftShoulderAngle);
+
+    }
+
+    if (!isLeftShoulder) {
+      
+      if (rightShoulderAngle >= 90) {
+        rightShoulderAngle = 30;
+      }
+      else {
+        rightShoulderAngle = rightShoulderAngle + SHOULDER_ANGLE_INCREASE;
+      }
+
+    return "RIGHT-SHOULDER " + String(rightShoulderAngle);
+    }
+
+  }
+  return "";
+}
+
+
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+  
+  if (mySerial.available()) {
+    Serial.print(mySerial.read());
+  }
+  mySerial.print(Serial.read());
+  mySerial.write(Serial.read());
+
+  pinMode(LEFT_CTRL_BUT,INPUT);
+  pinMode(RIGHT_CTRL_BUT,INPUT);
+  pinMode(HUM_TO_VEHIC_BUT,INPUT);
+
+  digitalWrite(LEFT_CTRL_BUT,HIGH);
+  digitalWrite(RIGHT_CTRL_BUT,HIGH);
+
+  Serial.begin(38400);
   Serial.println("Started!");
 
 }
 
 void loop() {
-  
 
+  String transformCmd = "";
+  if (digitalRead(HUM_TO_VEHIC_BUT)) {
+    if (isHumanoid) {
+      transformCmd = "HUM-TO-VEHIC";
+    }
+    else {
+      transformCmd = "VEHIC-TO-HUM";
+    }
+    isHumanoid = !isHumanoid;
+  }
+  
   int movX = analogRead(MOVEMENT_CTRL_HORZ);
   int movY = analogRead(MOVEMENT_CTRL_VERT);
-
 
   String horzCmd = determineMovementHorz(movX);
   String vertCmd = determineMovementVert(movY);
 
-  Serial.println(horzCmd);
-  Serial.println(vertCmd);
+  int leftCtrlX = analogRead(LEFT_CTRL_HORZ);
+  int leftCtrlY = analogRead(LEFT_CTRL_VERT);
 
-  delay(2000);
+  String leftArmCmd = determineArmMovement("LEFT",leftCtrlX);
+  String leftElbowCmd = determineElbowMovement("LEFT",leftCtrlY);
 
+  int rightCtrlX = analogRead(RIGHT_CTRL_HORZ);
+  int rightCtrlY = analogRead(RIGHT_CTRL_VERT);
 
- 
+  String rightArmCmd = determineArmMovement("RIGHT",rightCtrlX);
+  String rightElbowCmd = determineElbowMovement("RIGHT",rightCtrlY);
+
+  int leftCtrlBut = digitalRead(LEFT_CTRL_BUT);
+  int rightCtrlBut = digitalRead(RIGHT_CTRL_BUT);
+
+  bool isLeft = true;
+
+  String leftShoulderCmd = determineShoulderMovement(isLeft,leftCtrlBut);
+
+  isLeft = false;
+
+  String rightShoulderCmd = determineShoulderMovement(isLeft,rightCtrlBut);
 
   
+  if (!horzCmd.equals("")) {
+  }
 
+  if (!vertCmd.equals("")) {
+  }
+
+  if (!leftArmCmd.equals("")) {
+  }
+
+  if (!leftElbowCmd.equals("")) {
+  }
+
+  if (!leftShoulderCmd.equals("")) {
+  }
+
+  if (!rightShoulderCmd.equals("")) {
+  }
+
+  if (!rightArmCmd.equals("")) {
+  }
+
+  if (!rightElbowCmd.equals("")) {
+  }
+
+  if (!transformCmd.equals("")) {
+
+  }
 
 
 }
