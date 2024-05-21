fa2effe6e6bd7d1b0152a60b9c99bd7865799b0d
Mehmet <tekmanmehmetyasar@protonmail.com>
2024-05-21 14:27:01 +0300

    Added music logic.

--- robot_v2.ino
+++ robot_v2.ino
@@ -16,6 +16,27 @@ TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR TH
 #include <SoftwareSerial.h>
 #include "ucode.h"
 
+#define B 12
+
+// These are the frequencies of the notes 
+#define C4 261.626
+#define D4 293.665
+#define E4 329.628
+#define F4 349.228
+#define G4 391.995
+#define GSharp4 415.305 
+#define a4 440
+#define B4 493.883
+#define C5 523.251
+#define D5 587.33
+
+// This determines how many miliseconds a quarter note sounds
+#define QUARTER_DUR 250
+
+const float EIGHTH_DUR = QUARTER_DUR / 2;
+const float HALF_DUR = QUARTER_DUR * 2;
+const float WHOLE_DUR = QUARTER_DUR * 4;
+
 #define ULTRA_SONIC_ID 10
 
 // If there's an obstacle closer than DISTANCE_LIMIT, robot won't move forward. Only in vehicle mode.
@@ -51,12 +72,120 @@ void setup() {
     if (protocolRunState == false) {
       BT.begin(9600);
       Serial.begin(9600);
+      pinMode(B, OUTPUT);
+
     }
 }
 
+void playNote(String note, int noteValue) {
+  
+  float freq;
+  float dur;
+  
+  if (note.equals("C4")) {freq = C4;}
+  else if (note.equals("D4")) {freq = D4;}
+  else if (note.equals("E4")) {freq = E4;}
+  else if (note.equals("F4")) {freq = F4;}
+  else if (note.equals("G4")) {freq = G4;}
+  else if (note.equals("GSharp4")) {freq = GSharp4;}
+  else if (note.equals("A4")) {freq = a4;}
+  else if (note.equals("B4")) {freq = B4;}
+  else if (note.equals("C5")) {freq = C5;}
+  else if (note.equals("D5")) {freq = D5;} 
+  
+  if (noteValue == 4) {dur = QUARTER_DUR;}
+  else if (noteValue == 8) {dur = EIGHTH_DUR;}
+  else if (noteValue == 2) {dur = HALF_DUR;}
+  else if (noteValue == 1) {dur = WHOLE_DUR;}
+  else if (noteValue == 84) {dur = QUARTER_DUR + EIGHTH_DUR;}
+  else if (noteValue == 42) {dur = QUARTER_DUR + HALF_DUR;}
+  else if (noteValue == 82) {dur = EIGHTH_DUR + HALF_DUR;}
+  
+  tone(B,freq);
+  delay(dur);
+  noTone(B);
+  delay(100);
+  
+}
+
 void loop() {
+
+
+  String notes[] = {"E4","E4","D4","C4",
+                    "D4","E4","F4","E4","D4","C4",
+                    "F4","F4","E4","D4","E4",
+                    "F4","F4","F4","G4","F4","D4",
+                    "E4","E4","D4","C4",
+                    "E4","E4","E4","D4","E4",
+                    "E4","E4","D4","C4",
+                    "D4","E4","F4","E4","D4","C4",
+                    "F4","F4","E4","D4","E4",
+                    "F4","F4","F4","G4","F4","D4",
+                    "E4","E4","D4","C4",
+                    "E4","E4","E4","D4","E4",
+                   	"A4","C5","B4","C5","A4",
+                    "C5","B4","C5",
+                    "A4","C5","C5","B4","C5","B4","C5",
+                    "D5","C5","D5","C5","C5","B4",
+                    "B4","B4","A4","B4","GSharp4",
+                    "B4","A4","B4",
+                    "B4","B4","A4","B4",
+                    "A4","B4",
+                    "C5","B4","C5","B4","B4","A4",
+                    "A4","C5","B4","C5","A4",
+                    "C5","B4","C5",
+                    "A4","C5","C5","B4","C5","B4","C5",
+                    "D5","C5","D5","C5","C5","B4",
+                    "B4","B4","A4","B4","GSharp4",
+                    "B4","A4","B4",
+                    "B4","B4","A4","B4",
+                    "A4","B4",
+                    "C5","B4","C5","B4","B4","A4",        
+                   };
+  int durations[] = {8,8,8,84,
+                     8,4,8,4,8,4,  
+                     8,8,8,84,8,
+                     4,8,4,8,8,8,
+                     8,8,8,82,
+                     4,4,4,4,1,
+                     8,8,8,84,
+                     8,4,8,4,8,4,
+                     8,8,8,84,8,
+                     4,8,4,8,8,8,
+                     8,8,8,82,
+                     4,4,4,4,1,
+                     8,4,8,4,4,
+                     4,8,42,              
+                     8,8,8,8,4,8,8,
+                     4,8,4,8,8,8,
+                     8,8,8,4,4,
+                     4,8,82,
+                     8,8,8,4,
+                     8,8,
+                     4,8,4,8,8,8,
+                     8,4,8,4,4,
+                     4,8,42,
+                     8,8,8,8,4,8,8,
+                     4,8,4,8,8,8,
+                     8,8,8,4,4,
+                     4,8,82,
+                     8,8,8,4,
+                     8,8,
+                     4,8,4,8,8,8,
+                    };
+
+  int numberOfNotes = sizeof(durations) / sizeof(durations[0]);
+
+
     protocol();
     if (protocolRunState == false) {
+
+      button1.Update();
+      if(button1.clicks == 1) {
+        for (int i=0;i<numberOfNotes;i++) {
+          playNote(notes[i],durations[i]);
+        }
+      }
 
       float distance = readUltrasonicDistance(ULTRA_SONIC_ID);
 
@@ -183,7 +312,6 @@ void loop() {
             int desiredAngle = currentAngle + isHigh * MOVEMENT_ANGLE;
             setServoAngle(LEFT_SHOULDER_ID,desiredAngle,SERVO_MOVEMENT_DURATION);
 
-            previousLeftShoulderAngle = currentAngle;
 
           }
 
@@ -208,7 +336,6 @@ void loop() {
             int desiredAngle = currentAngle + isHigh * MOVEMENT_ANGLE;
             setServoAngle(RIGHT_SHOULDER_ID,desiredAngle,SERVO_MOVEMENT_DURATION);
 
-            previousRightShoulderAngle = currentAngle;
 
         }
 
